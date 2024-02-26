16b64c8119683155ad583a3a2e796dc04f1366b1
Roni McGuinness <roniemail1@gmail.com>
2024-02-26 16:08:44 +0000

    4th

--- src/app/NotFoundPage.tsx
+++ src/app/NotFoundPage.tsx
@@ -5,7 +5,7 @@ const NotFoundPage: NextPage = () => {
   return (
     <div>
       <h1>404 - Page Not Found</h1>
-      <p>Oops! Looks like you've encountered a page that doesn't exist.</p>
+      <p>You&apos;ve encountered a page that doesn&apos;t exist.</p>
       <p>
         Go back to{" "}
         <Link href="/">
